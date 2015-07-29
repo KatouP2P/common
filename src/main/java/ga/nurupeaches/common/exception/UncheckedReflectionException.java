b7f1073def2f1eae0668fff1d5060f79c39f412e
Tsunko <cirnothegenius@aol.com>
2015-07-28 23:09:02 -0700

    Serialization expansion! It actually works now! Supports Lists and Maps (special interactions with ArrayList and HashMap are required because of how their default constructor work).

--- src/main/java/ga/nurupeaches/common/exception/UncheckedReflectionException.java
+++ src/main/java/ga/nurupeaches/common/exception/UncheckedReflectionException.java
@@ -5,6 +5,9 @@ public class UncheckedReflectionException extends RuntimeException {
     public UncheckedReflectionException(String s, Throwable t){
         super(s, t);
     }
+    public UncheckedReflectionException(String s){
+        super(s);
+    }
 
     public UncheckedReflectionException(Throwable t){
         super(t);
